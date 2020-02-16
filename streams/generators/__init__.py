39628a86f30cef77aee43f8a07e8be0a059830c3
Ashraf-T <tahmasbi@iastate.edu>
2020-02-15 19:16:11 -0600

    remove tornado generators

--- streams/generators/__init__.py
+++
@@ -1,8 +0,0 @@
-from streams.generators.sine1_stream import SINE1
-from streams.generators.sine2_stream import SINE2
-from streams.generators.mixed_stream import MIXED
-from streams.generators.stagger_stream import STAGGER
-from streams.generators.sea_stream import SEA
-from streams.generators.circles_stream import CIRCLES
-from streams.generators.led_stream import LEDConceptDrift
-
